25bd439c7a7b5b5dadb40aab990b37986d44c09d
Alex You Shao <alexshao1993@gmail.com>
2022-05-28 21:45:29 -0700

    Added more org mode configs

--- config.el
+++ config.el
@@ -18,9 +18,6 @@
 ;; show line number
 (setq display-line-numbers-type t)
 
-;; set org mode root
-(setq org-directory "~/org/")
-
 ;; set project root
 (setq projectile-project-search-path '("~/the-rust-book", "~/.doom.d"))
 
@@ -29,11 +26,14 @@
   (setq rustic-lsp-server 'rls))
 
 ;; org mode
+
+;; set org mode root
+(setq org-directory "~/org/")
+(setq org-agenda-files '("~/org"))
+
 (defun ays/org-mode-setup ()
-  (org-indent-mode 1)
   (variable-pitch-mode 1)
-  (visual-line-mode 1)
-  (mixed-pitch-mode 1))
+  (company-mode 0))
 
 (defun ays/org-font-setup ()
   ;; Set faces for heading levels
@@ -58,15 +58,65 @@
   (set-face-attribute 'org-list-dt nil          :inherit '(shadow fixed-pitch))
   (set-face-attribute 'org-todo nil             :inherit '(shadow fixed-pitch))
   (set-face-attribute 'org-done nil             :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-date nil             :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-drawer nil           :inherit '(shadow fixed-pitch))
   ;; Enlarge title
   (set-face-attribute 'org-document-title nil   :inherit 'variable-pitch :height 2.0))
 
 (use-package! org
   :hook (org-mode . ays/org-mode-setup)
   :config
-  (setq org-ellipsis " ▾"
-        display-line-numbers-type nil)
+  ;; replace ... when folding
+  (setq org-ellipsis " ▾")
+
+  ;; disable line numbers
+  (setq display-line-numbers-type nil)
+
+  ;; agenda loggin
+  (setq org-agenda-start-with-log-mode t
+        org-log-done 'time
+        org-log-into-drawer t)
+
+  ;; todo
+  (setq org-todo-keywords
+    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+      (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "INPROG(p)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+  ;; tag
+  (setq org-tag-alist
+    '((:startgroup)
+       ; Put mutually exclusive tags here
+       (:endgroup)
+       ("@errand" . ?E)
+       ("@home" . ?H)))
+
+  ;; captures
+  (setq org-capture-templates
+    `(("t" "Tasks / Projects")
+      ("tt" "Task" entry (file+olp "~/org/tasks.org" "Inbox")
+           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+
+      ("j" "Journal Entries")
+      ("jj" "Journal" entry
+           (file+olp+datetree "~/org/journal.org")
+           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+           ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
+           :clock-in :clock-resume
+           :empty-lines 1)
+      ("jm" "Meeting" entry
+           (file+olp+datetree "~/org/journal.org")
+           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+           :clock-in :clock-resume
+           :empty-lines 1)))
+
+  ;; fonts
   (ays/org-font-setup))
+
+(use-package! mixed-pitch
+  :hook (org-mode . mixed-pitch-mode)
+  :config
+  (setq mixed-pitch-set-Height t)
+  (setq mixed-pitch-face 'variable-pitch))
 
 ;; set bullet style for headings
 (use-package! org-bullets
